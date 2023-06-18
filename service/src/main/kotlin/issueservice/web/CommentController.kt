b08deb89527a83b54d86031d34740d6c31d8b2ea
MintKim1990 <rlaalsxo1990@gmail.com>
2023-06-19 01:30:02 +0900

    feat: 코멘트 삭제 API 기능 추가

--- service/src/main/kotlin/issueservice/web/CommentController.kt
+++ service/src/main/kotlin/issueservice/web/CommentController.kt
@@ -3,11 +3,14 @@ package issueservice.web
 import issueservice.config.AuthUser
 import issueservice.model.CommentRequest
 import issueservice.service.CommentService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -29,5 +32,13 @@ class CommentController(
         @PathVariable id: Long,
         @RequestBody request: CommentRequest
     ) = commentService.edit(id, authUser.userId, request)
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(
+        authUser: AuthUser,
+        @PathVariable issueId: Long,
+        @PathVariable id: Long
+    ) = commentService.delete(issueId, id, authUser.userId)
 
 }
