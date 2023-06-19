08296569e8b352abcdf9f35940ef9a93974f2c59
MintKim1990 <rlaalsxo1990@gmail.com>
2023-06-20 08:13:27 +0900

    feat: 사용자 인증서비스 로그인, 로그아웃 기능 추가

--- user-service/src/main/kotlin/userservice/controller/UserController.kt
+++ user-service/src/main/kotlin/userservice/controller/UserController.kt
@@ -1,9 +1,15 @@
 package userservice.controller
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import userservice.model.AuthToken
+import userservice.model.SignInRequest
+import userservice.model.SignInResponse
 import userservice.model.SignUpRequest
 import userservice.service.UserService
 
@@ -16,6 +22,17 @@ class UserController (
     @PostMapping("/signup")
     suspend fun signUp(@RequestBody request: SignUpRequest) {
         userService.signUp(request)
+    }
+
+    @PostMapping("/signin")
+    suspend fun signIn(@RequestBody signInRequest: SignInRequest) : SignInResponse {
+        return userService.signIn(signInRequest)
+    }
+
+    @DeleteMapping("/logout")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    suspend fun logout(@AuthToken token: String) {
+        userService.logout(token)
     }
 
 }
--- user-service/src/main/kotlin/userservice/service/UserService.kt
+++ user-service/src/main/kotlin/userservice/service/UserService.kt
@@ -1,16 +1,30 @@
 package userservice.service
 
 import org.springframework.stereotype.Service
+import userservice.config.JwtProperties
 import userservice.domain.entity.User
 import userservice.domain.repository.UserRepository
+import userservice.exception.PasswordNotMatchedException
 import userservice.exception.UserExistException
+import userservice.exception.UserNotFoundException
+import userservice.model.SignInRequest
+import userservice.model.SignInResponse
 import userservice.model.SignUpRequest
 import userservice.utils.BCryptUtils
+import userservice.utils.JwtClaim
+import userservice.utils.JwtUtils
+import java.time.Duration
 
 @Service
 class UserService (
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val jwtProperties: JwtProperties,
+    private val cacheManager: CoroutinCacheManager<User>
 ) {
+
+    companion object {
+        private val CACHE_TTL = Duration.ofMinutes(1)
+    }
 
     suspend fun signUp(signUpRequest: SignUpRequest) {
 
@@ -28,6 +42,35 @@ class UserService (
             userRepository.save(user)
         }
 
+    }
+
+    suspend fun signIn(signInRequest: SignInRequest): SignInResponse {
+        return with(userRepository.findByEmail(signInRequest.email) ?: throw UserNotFoundException()) {
+            if (!BCryptUtils.verify(signInRequest.password, password)) {
+                throw PasswordNotMatchedException()
+            }
+
+            val jwtClaim = JwtClaim(
+                userId = id!!,
+                email = email,
+                profileUrl = profileUrl,
+                username = username
+            )
+
+            val token = JwtUtils.createToken(jwtClaim, jwtProperties)
+
+            cacheManager.awaitPut(key = token, value = this, ttl = CACHE_TTL)
+
+            SignInResponse(
+                email = email,
+                username = username,
+                token = token
+            )
+        }
+    }
+
+    suspend fun logout(token: String) {
+        cacheManager.awaitEvict(token)
     }
 
 }
