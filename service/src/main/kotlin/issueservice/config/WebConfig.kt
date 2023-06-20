4fecd78131dbb3cba80d72898b23506d0bbe1e48
MintKim1990 <rlaalsxo1990@gmail.com>
2023-06-20 09:16:57 +0900

    fix: 이슈관리 서비스 AuthUser 연동 기능 추가

--- service/src/main/kotlin/issueservice/config/WebConfig.kt
+++ service/src/main/kotlin/issueservice/config/WebConfig.kt
@@ -1,5 +1,9 @@
 package issueservice.config
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import issueservice.exception.UnauthorizedException
+import kotlinx.coroutines.runBlocking
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
@@ -7,6 +11,8 @@ import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
 
 @Configuration
@@ -21,7 +27,9 @@ class WebConfig(
 }
 
 @Component
-class AuthUserHandlerArgumentResolver : HandlerMethodArgumentResolver {
+class AuthUserHandlerArgumentResolver(
+    @Value("\${auth.url}") val authUrl: String
+) : HandlerMethodArgumentResolver {
 
     // 인증 처리를 위한 리졸버로 요청 메서드 파라미터에 AuthUser클래스가 존재하는지 체크하는 로직
     override fun supportsParameter(parameter: MethodParameter): Boolean =
@@ -33,16 +41,24 @@ class AuthUserHandlerArgumentResolver : HandlerMethodArgumentResolver {
         webRequest: NativeWebRequest,
         binderFactory: WebDataBinderFactory?
     ): Any? {
-        //TODO 인증로직 추가 예정
-        return AuthUser(
-            userId = 1,
-            username = "테스트"
-        )
+
+        val authHeader = webRequest.getHeader("Authorization") ?: throw UnauthorizedException()
+
+        return runBlocking {
+            WebClient.create()
+                .get()
+                .uri(authUrl)
+                .header("Authorization", authHeader) // 토큰 전달
+                .retrieve()
+                .awaitBody<AuthUser>()
+        }
     }
 }
 
 data class AuthUser(
+    @JsonProperty("id")
     val userId: Long,
     val username: String,
+    val email: String,
     val profileUrl: String? = null
 )
